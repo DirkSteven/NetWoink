15350138c7ce1d723e6a7a6ccfbbc10a877b1877
Dirk Javier <dirkjaviermvp@gmail.com>
2024-04-30 02:52:17 +0800

    InitializeWindow UI and Functionality Skeleton Fix

--- NetworkMonitorApp/NetworkMonitor/InitializeWindow.xaml.cs
+++ NetworkMonitorApp/NetworkMonitor/InitializeWindow.xaml.cs
@@ -1,37 +1,111 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace NetworkMonitor
 {
-    /// <summary>
-    /// Interaction logic for InitializeWindow.xaml
-    /// </summary>
+    public class NetworkAdapterHelper
+    {
+        public static List<NetworkInterface> GetNetworkAdapters()
+        {
+            return NetworkInterface.GetAllNetworkInterfaces().ToList();
+        }
+    }
+
     public partial class InitializeWindow : Window
     {
+        public delegate void OKButtonClickedEventHandler(object sender, NetworkInterface selectedAdapter);
+
+
+        public event OKButtonClickedEventHandler OKButtonClicked;
+
+
         public InitializeWindow()
         {
             InitializeComponent();
+            PopulateComboBox();
+        }
+
+        private void PopulateComboBox()
+        {
+            List<NetworkInterface> adapters = NetworkAdapterHelper.GetNetworkAdapters();
+
+            foreach (var adapter in adapters)
+            {
+                ComboBoxItem item = new ComboBoxItem();
+                item.Content = adapter.Name;
+                comboBox.Items.Add(item);
+            }
         }
 
         private void OKButton_Click(object sender, RoutedEventArgs e)
         {
+            if (comboBox.SelectedItem != null)
+            {
+                string selectedAdapterName = ((ComboBoxItem)comboBox.SelectedItem).Content.ToString();
+                NetworkInterface selectedAdapter = NetworkAdapterHelper.GetNetworkAdapters()
+    .FirstOrDefault(adapter => adapter.Name == selectedAdapterName);
 
+
+                // Raise the event with the selected network adapter
+                OKButtonClicked?.Invoke(this, selectedAdapter);
+
+                // Close the window
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Please select a network adapter.");
+            }
+        }
+
+
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox.SelectedItem != null)
+            {
+                string selectedAdapterName = ((ComboBoxItem)comboBox.SelectedItem).Content.ToString();
+                NetworkInterface selectedAdapter = NetworkInterface.GetAllNetworkInterfaces()
+                    .FirstOrDefault(adapter => adapter.Name == selectedAdapterName);
+
+                if (selectedAdapter != null)
+                {
+                    IPInterfaceProperties adapterProperties = selectedAdapter.GetIPProperties();
+                    IPAddress ipAddress = adapterProperties.UnicastAddresses.FirstOrDefault()?.Address;
+                    PhysicalAddress macAddress = selectedAdapter.GetPhysicalAddress();
+                    GatewayIPAddressInformation gateway = adapterProperties.GatewayAddresses.FirstOrDefault();
+                    string ssid = selectedAdapter.Name; // Assuming SSID is the same as adapter name for wired connections
+                    string driverVersion = selectedAdapter.Description;
+
+                    // Update labels with adapter information
+                    UpdateLabelContent(nicTypeLabel, selectedAdapter.NetworkInterfaceType.ToString(), "NIC Type: ");
+                    UpdateLabelContent(ipAddressLabel, ipAddress?.ToString(), "IP Address: ");
+                    UpdateLabelContent(macAddressLabel, macAddress?.ToString(), "MAC Address: ");
+                    UpdateLabelContent(gatewayLabel, gateway?.Address?.ToString(), "Gateway: ");
+                    UpdateLabelContent(networkSSIDLabel, ssid, "Network SSID: ");
+                    UpdateLabelContent(driverVersionLabel, driverVersion, "Driver Version: ");
+                }
+            }
+        }
+
+        private void UpdateLabelContent(Label label, string content, string labelText)
+        {
+            if (label != null)
+            {
+                label.Content = labelText + (string.IsNullOrEmpty(content) ? "N/A" : content);
+            }
         }
 
         private void QuitButton_Click(object sender, RoutedEventArgs e)
         {
-
+            this.Close();
         }
+
+
     }
 }
--- NetworkMonitorApp/NetworkMonitor/MainWindow.xaml.cs
+++ NetworkMonitorApp/NetworkMonitor/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.Net.NetworkInformation;
+using System.Net;
+using System.Reflection.Emit;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace NetworkMonitor
 {
@@ -16,17 +20,46 @@ namespace NetworkMonitor
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
         public MainWindow()
         {
             InitializeComponent();
             InitializeWindow initWindow = new InitializeWindow();
-            initWindow.Show();
+            initWindow.OKButtonClicked += InitializeWindow_OKButtonClicked; // Subscribe to the OKButtonClicked event
+            initWindow.ShowDialog(); // Show as a modal dialog
         }
+
+        private void InitializeWindow_OKButtonClicked(object sender, NetworkInterface selectedAdapter)
+        {
+            // This method will be called when OK button is clicked in InitializeWindow.xaml
+            // You can add your logic here to handle the selected network adapter
+            // For now, let's just display the selected network adapter's information
+            DisplayDeviceInfo(selectedAdapter);
+        }
+
+        private void DisplayDeviceInfo(NetworkInterface adapter)
+        {
+            // Assuming you have Labels named ipLabel, macLabel, nameLabel, and vendorLabel in your MainWindow.xaml
+
+            IPInterfaceProperties adapterProperties = adapter.GetIPProperties();
+            IPAddress ipAddress = adapterProperties.UnicastAddresses.FirstOrDefault()?.Address;
+            PhysicalAddress macAddress = adapter.GetPhysicalAddress();
+            string name = adapter.Name;
+            string vendor = adapter.Description;
+
+            ipLabel.Content = "IP: " + (ipAddress != null ? ipAddress.ToString() : "N/A");
+            macLabel.Content = "MAC: " + (macAddress != null ? macAddress.ToString() : "N/A");
+            nameLabel.Content = "NAME: " + name;
+            vendorLabel.Content = "VENDOR: " + vendor;
+        }
+
 
         private void ScanButton_Click(object sender, RoutedEventArgs e)
         {
-
+            // This method will be called when the Scan button is clicked
+            // You can add your scanning logic here
+            // For now, let's just display information about the selected network adapter
+            // You can call DisplayDeviceInfo with the selected adapter from InitializeWindow
         }
 
         private void RefreshButton_Click(object sender, RoutedEventArgs e)
