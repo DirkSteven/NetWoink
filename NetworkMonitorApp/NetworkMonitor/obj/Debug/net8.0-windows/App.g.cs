7a4a51a452995d13da6a34308c400d1578a6450e
Dirk Javier <dirkjaviermvp@gmail.com>
2024-05-08 13:36:55 +0800

    Collective Merge

--- NetworkMonitorApp/NetworkMonitor/obj/Debug/net8.0-windows/App.g.cs
+++ NetworkMonitorApp/NetworkMonitor/obj/Debug/net8.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace NetworkMonitor {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace NetworkMonitor {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.4.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public static void Main() {
             NetworkMonitor.App app = new NetworkMonitor.App();
             app.InitializeComponent();
